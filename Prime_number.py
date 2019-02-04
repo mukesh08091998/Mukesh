bba05c30cff06fa8f50cad11cba7d6e960f5c1b7
mukesh08091998 <46933405+mukesh08091998@users.noreply.github.com>
2019-02-04 16:23:37 +0530

    Create Prime_number.py

---
+++ Prime_number.py
@@ -0,0 +1,9 @@
+x = int(input())  
+y = int(input())
+for num in range(x+1,y):  
+   if num > 1:  
+       for i in range(2,num):  
+           if (num % i) == 0:  
+               break  
+       else:  
+           print(num) 
